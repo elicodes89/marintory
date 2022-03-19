96138bbc94373ddbf4a75972a2260f6a3fffa9cf
elicodes89 <elicodes89@gmail.com>
2022-03-19 16:50:15 +0000

    able to show manufacturers and delete by id and all

--- console.py
+++ console.py
@@ -1,14 +1,29 @@
 import pdb
 from models.user import User
+from models.manufacturer import Manufacturer
+# from models.product import Product
 
 import repositories.user_repository as user_repository
+# import repositories.product_repository as product_repository
+import repositories.manufacturer_repository as manufacturer_repository
 
 user_repository.delete_all()
+manufacturer_repository.delete_all()
+# product_repository.delete_all()
 
 user_1 = User('Elisol', 'Shop Manager')
 user_repository.save(user_1)
+
 user_2 = User('Marcos' , 'Assistant')
 user_repository.save(user_2)
+
+manufacturer1 = Manufacturer('Marine Accesories,LTD.', 'marcos@marineaccesories.com', '01312225555', 'Accesories')
+manufacturer_repository.save(manufacturer1)
+manufacturer2 = Manufacturer('Marine Fish&Coral,LTD.', 'carolina@marinefishcoral.com', '01312225556', 'Live Stock')
+manufacturer_repository.save(manufacturer2)
+manufacturer3 = Manufacturer('Food for your stock,LTD.', 'mariana@stockfood.com', '01312225557', 'Food')
+manufacturer_repository.save(manufacturer3)
+
 
 
 pdb.set_trace()
--- repositories/manufacturer_repository.py
+++ repositories/manufacturer_repository.py
@@ -2,3 +2,33 @@ from db.run_sql import run_sql
 from models.manufacturer import Manufacturer
 from models.user import User
 from models.product import Product
+
+import repositories.manufacturer_repository as manufacturer_repository
+import repositories.product_repository as product_repository
+
+
+def save(manufacturer):
+    sql = "INSERT INTO manufacturers ( name, email, contact_number, category) VALUES ( %s, %s, %s, %s ) RETURNING id"
+    values = [manufacturer.name, manufacturer.email, manufacturer.contact_number, manufacturer.category]
+    results = run_sql( sql, values )
+    manufacturer.id = results[0]['id']
+    return manufacturer
+
+def select_all():
+    manufacturers = []
+    sql = "SELECT * FROM manufacturers"
+    results = run_sql(sql)
+    for row in results:
+        manufacturer = Manufacturer(row['name'], row['email'], row['contact_number'], row['category'], row['id'])
+        manufacturers.append(manufacturer)
+    return manufacturers
+
+def delete_all():
+    sql = "DELETE FROM manufacturers"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM manufacturers WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
