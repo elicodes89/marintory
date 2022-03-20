02055dc07e913fcffca4323836bef63540935334
elicodes89 <elicodes89@gmail.com>
2022-03-20 13:48:23 +0000

    managed to make users login form to work. it is displaying the form and when clicking login it is displaying the users name and category

--- controllers/users_controller.py
+++ controllers/users_controller.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.user import User
-
 import repositories.user_repository as user_repository
 
 users_blueprint = Blueprint("users", __name__)
@@ -10,6 +9,17 @@ users_blueprint = Blueprint("users", __name__)
 def users():
     users = user_repository.select_all()
     return render_template("users/index.html", users = users)
+
+@users_blueprint.route("/users/login", methods = ['POST'])
+def login():
+    name = request.form['name']
+    category = request.form['category']
+    user = User(name, category)
+    # user1 = User(name, category)
+    # user2 = User(name, category)
+    saved_user = user_repository.save(user)
+
+    return render_template("/users/login.html", user=saved_user)
 
 @users_blueprint.route("/users/<id>")
 def show(id):
